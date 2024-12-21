49cd8474a744ca6fa799eddb9e8c31bda59a3292
Ivan Monastyrev <ikloster@yandex.ru>
2024-12-22 02:26:31 +0300

    feat: add router type

--- src/modules/about/about.routes.ts
+++ src/modules/about/about.routes.ts
@@ -1,15 +1,17 @@
 import { RouteRecordRaw } from 'vue-router';
 import AboutPage from './AboutPage.vue';
+import { Page } from '@/router.type';
 
-export const ABOUT_PAGE = {
-  PATH: '/about',
-  NAME: 'about-page',
+export const ABOUT_PAGE: Page = {
+  path: '/about',
+  name: 'about-page',
+  title: 'About',
 } as const;
 
 export const routes: RouteRecordRaw[] = [
   {
-    path: ABOUT_PAGE.PATH,
-    name: ABOUT_PAGE.NAME,
+    path: ABOUT_PAGE.path,
+    name: ABOUT_PAGE.name,
     component: AboutPage,
   },
 ];
--- src/modules/home/home.routes.ts
+++ src/modules/home/home.routes.ts
@@ -1,15 +1,17 @@
 import { RouteRecordRaw } from 'vue-router';
 import HomePage from './HomePage.vue';
+import { Page } from '@/router.type';
 
-export const HOME_PAGE = {
-  PATH: '/',
-  NAME: 'home-page',
+export const HOME_PAGE: Page = {
+  path: '/',
+  name: 'home-page',
+  title: 'Home',
 } as const;
 
 export const routes: RouteRecordRaw[] = [
   {
-    path: HOME_PAGE.PATH,
-    name: HOME_PAGE.NAME,
+    path: HOME_PAGE.path,
+    name: HOME_PAGE.name,
     component: HomePage,
   },
 ];
