535ec9ca10ce2a2c09fa8d479c9a3cfd63378ca6
Ivan Monastyrev <ikloster@yandex.ru>
2024-12-21 17:09:39 +0300

    feat: add store to app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue';
-import App from './App.vue';
 import router from './router';
+import store from './store';
+import { App } from './modules/app';
 
 const setupApp = async () => {
   const app = createApp(App);
 
   app.use(router);
+  app.use(store);
   app.mount('#app');
 };
 
