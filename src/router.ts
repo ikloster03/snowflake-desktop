b16462bb05640fb3037f50b76f681070a65cd96d
Ivan Monastyrev <ikloster@yandex.ru>
2024-12-23 00:24:46 +0300

    chore: routes imports

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,10 @@
 import { aboutRoutes } from '@/modules/about';
+import { bookRoutes } from '@/modules/book';
 import { homeRoutes } from '@/modules/home';
+import { loreRoutes } from '@/modules/lore';
+import { projectionRoutes } from '@/modules/projection';
 import { settingsRoutes } from '@/modules/settings';
 import { createMemoryHistory, createRouter, RouteRecordRaw } from 'vue-router';
-import { bookRoutes } from './modules/book';
-import { loreRoutes } from './modules/lore';
-import { projectionRoutes } from './modules/projection';
 
 const routes: RouteRecordRaw[] = [
   ...homeRoutes,
