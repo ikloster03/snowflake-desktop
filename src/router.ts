89f2d3d2e534bcc709db97f340a4c3c0884667b7
Ivan Monastyrev <ikloster@yandex.ru>
2024-12-22 22:30:28 +0300

    feat: update routes

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,19 @@
-import { createMemoryHistory, createRouter, RouteRecordRaw } from 'vue-router';
 import { aboutRoutes } from '@/modules/about';
 import { homeRoutes } from '@/modules/home';
+import { settingsRoutes } from '@/modules/settings';
+import { createMemoryHistory, createRouter, RouteRecordRaw } from 'vue-router';
+import { bookRoutes } from './modules/book';
+import { loreRoutes } from './modules/lore';
+import { projectionRoutes } from './modules/projection';
 
-const routes: RouteRecordRaw[] = [...homeRoutes, ...aboutRoutes];
+const routes: RouteRecordRaw[] = [
+  ...homeRoutes,
+  ...aboutRoutes,
+  ...bookRoutes,
+  ...loreRoutes,
+  ...projectionRoutes,
+  ...settingsRoutes,
+];
 
 const router = createRouter({
   history: createMemoryHistory(),
