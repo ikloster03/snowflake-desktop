5278d4d7ebba603aa9e44946c067e5881184eae1
Ivan Monastyrev <ikloster@yandex.ru>
2024-12-23 00:41:44 +0300

    feat(settings): update theme select

--- src/modules/settings/SettingsPage.vue
+++ src/modules/settings/SettingsPage.vue
@@ -1,16 +1,49 @@
 <script lang="ts" setup>
-import { NCard } from 'naive-ui';
+import { NCard, NSelect } from 'naive-ui';
 import { useAppStore } from '@/modules/app';
+import { computed, ref } from 'vue';
+import { SelectBaseOption } from 'naive-ui/es/select/src/interface';
 
 const appStore = useAppStore();
+
+const themeOptions = ref<SelectBaseOption[]>([
+  {
+    label: 'Dark',
+    value: 'dark',
+  },
+  {
+    label: 'Light',
+    value: 'light',
+  },
+]);
+
+const defaultThemeOption = computed(() => {
+  const option: string | number | undefined = themeOptions.value.find(
+    (o) => o.value === appStore.theme.name
+  )?.value;
+
+  if (option === undefined) {
+    throw new Error('Невозможное состояние');
+  }
+
+  return option;
+});
+
+const handleUpdateTheme = (value: string) => {
+  if (value !== appStore.theme.name) {
+    appStore.toggleTheme();
+  }
+};
 </script>
 
 <template>
   <NCard>
     <h1>Settings</h1>
-    <a href="#" @click.prevent="appStore.toggleTheme"
-      >Change theme {{ appStore.theme.name }}</a
-    >
+    <NSelect
+      :defaultValue="defaultThemeOption"
+      :options="themeOptions"
+      @update:value="handleUpdateTheme"
+    />
   </NCard>
 </template>
 
