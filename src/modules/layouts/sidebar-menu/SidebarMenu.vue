82f9b2217d6ecdcbd98702a72eed196edd3cb78e
Ivan Monastyrev <ikloster@yandex.ru>
2024-12-22 22:30:47 +0300

    feat: upadte sidebar menu

--- src/modules/layouts/sidebar-menu/SidebarMenu.vue
+++ src/modules/layouts/sidebar-menu/SidebarMenu.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
 import { NIcon, NLayoutSider, NMenu } from 'naive-ui';
-import { Component, h } from 'vue';
+import { Component, h, ref } from 'vue';
 import {
+  Home20Filled as HomeIcon,
+  Settings20Filled as SettingsIcon,
   Book20Filled as BookIcon,
-  Person20Filled as PersonIcon,
+  Timeline20Filled as TimelineIcon,
+  PeopleCommunity20Filled as CharactersIcon,
 } from '@vicons/fluent';
+import { World as WorldIcon, Notes as NotesIcon } from '@vicons/tabler';
+
 import { RouterLink } from 'vue-router';
 import { HOME_PAGE } from '@/modules/home';
-import { ABOUT_PAGE } from '@/modules/about';
 import { Page } from '@/router.type';
+import { MenuMixedOption } from 'naive-ui/es/menu/src/interface';
+import { SETTINGS_PAGE } from '@/modules/settings';
+import { BOOK_PAGE } from '@/modules/book';
+import { LORE_PAGE } from '@/modules/lore';
+import {
+  CHARACTER_MAP_PAGE,
+  PLAN_PAGE,
+  TIMELINE_PAGE,
+} from '@/modules/projection/projection.routes';
 
 function renderIcon(icon: Component) {
   return () => h(NIcon, null, { default: () => h(icon) });
@@ -16,42 +29,64 @@ function renderIcon(icon: Component) {
 
 function renderLink(page: Page) {
   return () =>
-    h(
-      RouterLink,
-      {
-        to: {
-          name: page.name,
-        },
-      },
-      { default: () => page.title }
-    );
+    h(RouterLink, { to: { name: page.name } }, { default: () => page.title });
 }
 
-const menuOptions = [
+const menuOptions: MenuMixedOption[] = [
   {
     label: renderLink(HOME_PAGE),
-    key: 'hear-the-wind-sing1',
+    key: HOME_PAGE.name,
+    icon: renderIcon(HomeIcon),
+  },
+  {
+    label: renderLink(BOOK_PAGE),
+    key: BOOK_PAGE.name,
     icon: renderIcon(BookIcon),
   },
   {
-    label: renderLink(ABOUT_PAGE),
-    key: 'hear-the-wind-sing2',
-    icon: renderIcon(PersonIcon),
+    label: renderLink(PLAN_PAGE),
+    key: PLAN_PAGE.name,
+    icon: renderIcon(NotesIcon),
+  },
+  {
+    label: renderLink(LORE_PAGE),
+    key: LORE_PAGE.name,
+    icon: renderIcon(WorldIcon),
+  },
+  {
+    label: renderLink(TIMELINE_PAGE),
+    key: TIMELINE_PAGE.name,
+    icon: renderIcon(TimelineIcon),
+  },
+  {
+    label: renderLink(CHARACTER_MAP_PAGE),
+    key: CHARACTER_MAP_PAGE.name,
+    icon: renderIcon(CharactersIcon),
+  },
+  {
+    label: renderLink(SETTINGS_PAGE),
+    key: SETTINGS_PAGE.name,
+    icon: renderIcon(SettingsIcon),
   },
 ];
+
+const firstKeyMenu = menuOptions.at(0)?.key;
+
+if (firstKeyMenu === undefined) {
+  throw new Error('Невозможное состояние');
+}
 </script>
 
 <template>
   <NLayoutSider
     bordered
-    show-trigger
+    collapsed
     collapse-mode="width"
     :collapsed-width="64"
-    :width="240"
     :native-scrollbar="false"
-    style="max-height: 320px"
   >
     <NMenu
+      :default-value="firstKeyMenu"
       :collapsed-width="64"
       :collapsed-icon-size="22"
       :options="menuOptions"
