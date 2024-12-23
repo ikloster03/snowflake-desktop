1c927bebc906d56d9836964b5f8251f6a5f25b79
Ivan Monastyrev <ikloster@yandex.ru>
2024-12-24 01:24:47 +0300

    feat: update book page

--- src/modules/book/BookPage.vue
+++ src/modules/book/BookPage.vue
@@ -1,9 +1,17 @@
 <script lang="ts" setup>
 import { NCard } from 'naive-ui';
+import Author from './author/Author.vue';
+import Annotation from './annotation/Annotation.vue';
+import Synopsis from './synopsis/Synopsis.vue';
 </script>
 
 <template>
-  <NCard>Book</NCard>
+  <NCard>
+    <h1>Book</h1>
+    <Author />
+    <Annotation />
+    <Synopsis />
+  </NCard>
 </template>
 
 <style scoped></style>
