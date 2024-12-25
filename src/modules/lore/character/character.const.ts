a0690ebe0d13237a00497805b0d48dc98e7d4c1c
Ivan Monastyrev <ikloster@yandex.ru>
2024-12-26 01:44:05 +0300

    feat(char): add const

---
+++ src/modules/lore/character/character.const.ts
@@ -0,0 +1,19 @@
+export const CHARACTER_LEVEL = {
+  PRIMARY: 'primary',
+  SECONDARY: 'secondary',
+  TERTIARY: 'tertiary',
+} as const;
+
+export const MAIN_CHARACTER_TYPE = {
+  PROTAGONIST: 'protagonist',
+  ANTAGONIST: 'antagonist'
+} as const;
+
+export const SECONDARY_CHARACTER_TYPE = {
+  LOVE_INTEREST: 'love interest',
+  MENTOR: 'mentor',
+  CONFIDANTE: 'confidante',
+  PLOT_MOVER: 'plot mover',
+  FOIL: 'foil',
+  COMIC_RELIEF: 'comic relief',
+} as const;
