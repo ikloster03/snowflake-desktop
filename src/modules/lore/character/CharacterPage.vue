b8bf73eb8e220fa644fc5163ab902d196ff038aa
Ivan Monastyrev <ikloster@yandex.ru>
2024-12-26 01:43:26 +0300

    feat: init character components

---
+++ src/modules/lore/character/CharacterPage.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { NCard } from 'naive-ui';
+</script>
+
+<template>
+  <NCard>character</NCard>
+</template>
+
+<style scoped></style>
---
+++ src/modules/lore/character/NewCharacter.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { NCard } from 'naive-ui';
+</script>
+
+<template>
+  <NCard>New character</NCard>
+</template>
+
+<style scoped></style>
