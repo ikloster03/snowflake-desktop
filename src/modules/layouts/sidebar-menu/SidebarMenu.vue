b22dd425229cac8bb6b3be7907f93beffac7a44d
Ivan Monastyrev <ikloster@yandex.ru>
2025-01-09 00:48:25 +0300

    feat: add settings sidebar menu

--- src/modules/layouts/sidebar-menu/SidebarMenu.vue
+++ src/modules/layouts/sidebar-menu/SidebarMenu.vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
-import { NLayoutSider, NMenu } from 'naive-ui';
+import { Person32Filled as Person } from '@vicons/fluent';
+import { Menu2 as MenuIcon } from '@vicons/tabler';
+import {
+  NAvatar,
+  NButton,
+  NDrawer,
+  NDrawerContent,
+  NIcon,
+  NLayoutSider,
+  NMenu,
+  NSpace,
+} from 'naive-ui';
+import { ref } from 'vue';
 import menuOptions from './menu';
 
 const firstKeyMenu = menuOptions.at(0)?.key;
+const showSettings = ref(false);
 
 if (firstKeyMenu === undefined) {
   throw new Error('Невозможное состояние');
@@ -17,6 +30,17 @@ if (firstKeyMenu === undefined) {
     :collapsed-width="64"
     :native-scrollbar="false"
   >
+    <NButton
+      quaternary
+      style="width: 100%; height: 42px"
+      @click="showSettings = true"
+    >
+      <template #icon>
+        <NIcon>
+          <MenuIcon />
+        </NIcon>
+      </template>
+    </NButton>
     <NMenu
       :default-value="firstKeyMenu"
       :collapsed-width="64"
@@ -24,6 +48,18 @@ if (firstKeyMenu === undefined) {
       :options="menuOptions"
     />
   </NLayoutSider>
+  <NDrawer v-model:show="showSettings" :width="240" placement="left">
+    <NDrawerContent>
+      <NSpace align="center" justify="space-between">
+        <NAvatar round size="large">
+          <NIcon>
+            <Person />
+          </NIcon>
+        </NAvatar>
+      </NSpace>
+      <div>User's name</div>
+    </NDrawerContent>
+  </NDrawer>
 </template>
 
 <style scoped></style>
