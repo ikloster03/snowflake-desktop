f91a1b97c4fd57d467306a1408bba0b3150ff0e5
Ivan Monastyrev <ikloster@yandex.ru>
2025-01-12 02:32:03 +0300

    feat: update book page

--- src/modules/book/BookPage.vue
+++ src/modules/book/BookPage.vue
@@ -1,17 +1,48 @@
 <script lang="ts" setup>
-import { NCard } from 'naive-ui';
-import Author from './author/Author.vue';
-import Annotation from './annotation/Annotation.vue';
-import Synopsis from './synopsis/Synopsis.vue';
+import { NCard, NSpace } from 'naive-ui';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 </script>
 
 <template>
-  <NCard>
-    <h1>Book</h1>
-    <Author />
-    <Annotation />
-    <Synopsis />
-  </NCard>
+  <NSpace vertical>
+    <h1>{{ t('book-editor') }}</h1>
+    <div>
+      <h2>{{ t('authors') }}</h2>
+      <NSpace>
+        <NCard> + {{ t('add-new-author') }}</NCard>
+      </NSpace>
+    </div>
+    <div>
+      <h2>{{ t('books-series') }}</h2>
+      <NSpace>
+        <NCard> + {{ t('add-new-book') }}</NCard>
+        <NCard> + {{ t('add-new-series') }}</NCard>
+      </NSpace>
+    </div>
+  </NSpace>
 </template>
+
+<i18n>
+  {
+    "en": {
+      "book-editor": "Book's editor",
+      "authors": "Authors",
+      "books-series": "Books & Series of books",
+      "add-new-author": "Add an new author",
+      "add-new-book": "Add a new book",
+      "add-new-series": "Add a new series of books",
+    },
+    "ru": {
+      "book-editor": "Редактор книг",
+      "authors": "Авторы",
+      "books-series": "Книги и Серии книг",
+      "add-new-author": "Добавить автора",
+      "add-new-book": "Добавить книгу",
+      "add-new-series": "Добавить серию книг",
+    }
+  }
+</i18n>
 
 <style scoped></style>
