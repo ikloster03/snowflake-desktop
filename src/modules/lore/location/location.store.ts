26278e466001fc98879b95acb69b410b70d746c1
Ivan Monastyrev <ikloster@yandex.ru>
2025-01-19 04:08:35 +0300

    feat(location-store): add methods to manage locations in the private location store, including addLocation and updateLocation for enhanced location handling

--- src/modules/lore/location/location.store.ts
+++ src/modules/lore/location/location.store.ts
@@ -1,7 +1,7 @@
 import { PRIVATE_STORE_PREFIX } from '@/store.const';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
-import { ILocation } from './location.types';
+import type { ILocation } from './location.types';
 
 export const LOCATION_STORE = 'location';
 
@@ -10,8 +10,18 @@ export const usePrivateLocationStore = defineStore(
   () => {
     const locations = ref<ILocation[]>([]);
 
+    const addLocation = (location: ILocation) => {
+      locations.value.push(location);
+    };
+
+    const updateLocation = (index: number, location: ILocation) => {
+      locations.value[index] = location;
+    };
+
     return {
       locations,
+      addLocation,
+      updateLocation
     };
   }
 );
