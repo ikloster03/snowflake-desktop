9eccb6daa0ac83960f8cc7dfb519ad6daf4abf98
Ivan Monastyrev <ikloster@yandex.ru>
2025-02-13 00:08:49 +0300

    feat(settings): add theme and locale configuration constants

--- src/modules/settings/settings.const.ts
+++ src/modules/settings/settings.const.ts
@@ -1,7 +1,20 @@
 import { Page } from '@/router.type';
+import { enUS, ruRU, dateEnUS, dateRuRU, NLocale, NDateLocale } from 'naive-ui';
+import { Locale } from './settings.types';
 
 export const SETTINGS_PAGE: Page = {
   path: '/settings',
   name: 'settings-page',
   title: 'Settings',
+} as const;
+
+export const THEME_OPTIONS = ['light', 'dark'] as const;
+
+export const DATE_LOCALE_OPTIONS = ['en-US', 'ru-RU'] as const;
+
+const [EN_LOCALE, RU_LOCALE] = DATE_LOCALE_OPTIONS;
+
+export const DICT_LANG: Record<Locale, [NLocale, NDateLocale]> = {
+  [EN_LOCALE]: [enUS, dateEnUS],
+  [RU_LOCALE]: [ruRU, dateRuRU],
 } as const;
