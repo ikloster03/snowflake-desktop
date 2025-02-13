04510eb1f75b051ab4560b7bc48bc3730fa98308
Ivan Monastyrev <ikloster@yandex.ru>
2025-02-14 00:29:39 +0300

    feat(tauri): add Tauri store plugin for persistent storage

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -59,6 +59,7 @@ pub fn create_menu<R: Runtime>(app: &AppHandle<R>) -> Result<Menu<R>, tauri::Err
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         // .setup(|app| {
