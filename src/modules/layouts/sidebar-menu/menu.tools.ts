cc9977bd3cca64e8a3284669ff92c354ef6adcf9
Ivan Monastyrev <ikloster@yandex.ru>
2025-03-15 23:48:47 +0300

    feat: enhance menu link rendering to support internationalization

--- src/modules/layouts/sidebar-menu/menu.tools.ts
+++ src/modules/layouts/sidebar-menu/menu.tools.ts
@@ -2,12 +2,16 @@ import { Page } from '@/router.type';
 import { NIcon } from 'naive-ui';
 import { Component, h } from 'vue';
 import { RouterLink } from 'vue-router';
+import { useI18n } from 'vue-i18n';
 
 export const renderIcon = (icon: Component) => {
   return () => h(NIcon, null, { default: () => h(icon) });
 };
 
 export const renderLink = (page: Page) => {
-  return () =>
-    h(RouterLink, { to: { name: page.name } }, { default: () => page.title });
+  return () => {
+    const { t } = useI18n();
+
+    return h(RouterLink, { to: { name: page.name } }, { default: () => t(page.title) });
+  }
 };
