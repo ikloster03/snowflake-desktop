6a4a2da6b7e7341519c7b9816ab6d2102dcf28e0
Ivan Monastyrev <ikloster@yandex.ru>
2025-03-21 00:40:22 +0300

    feat: extend IProject interface to include project type using PROJECT_TYPE constant for enhanced project categorization

--- src/modules/project/project.types.ts
+++ src/modules/project/project.types.ts
@@ -1,4 +1,7 @@
+import { ValueOf } from "@/core";
 import { ProjectID } from "@/core/id";
+import { PROJECT_TYPE } from "./project.const";
+export type ProjectType = ValueOf<typeof PROJECT_TYPE>;
 
 export interface IProject {
   id: ProjectID;
@@ -8,4 +11,5 @@ export interface IProject {
   created: Date;
   updated: Date;
   isOpen: boolean;
+  type: ProjectType;
 }
