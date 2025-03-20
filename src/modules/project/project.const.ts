826a4652df293681f168e76e7409fc51d8cb55aa
Ivan Monastyrev <ikloster@yandex.ru>
2025-03-21 00:40:15 +0300

    feat: add PROJECT_TYPE constant to define project types for better project management

--- src/modules/project/project.const.ts
+++ src/modules/project/project.const.ts
@@ -1,5 +1,10 @@
 import { Page } from '@/router.type';
 
+export const PROJECT_TYPE = {
+  SINGLE_BOOK: 'single-book',
+  SERIES: 'series',
+} as const;
+
 export const PROJECT_PAGE: Page = {
   path: '/project',
   name: 'project-page',
