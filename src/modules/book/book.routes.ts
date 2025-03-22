b508440429570c73fad71e2e50dd1ab8f4f4f149
Ivan Monastyrev <ikloster@yandex.ru>
2025-03-22 23:42:02 +0300

    feat: update book routes to include BookEditor and SeriesEditor

--- src/modules/book/book.routes.ts
+++ src/modules/book/book.routes.ts
@@ -1,11 +1,23 @@
 import { RouteRecordRaw } from 'vue-router';
 import BookPage from './BookPage.vue';
-import { BOOK_PAGE } from './book.const';
+import BookEditor from './components/book-editor/BookEditor.vue';
+import SeriesEditor from './components/series-editor/SeriesEditor.vue';
+import { BOOK_PAGE, BOOK_EDITOR_PAGE, SERIES_EDITOR_PAGE } from './book.const';
 
 export const routes: RouteRecordRaw[] = [
   {
     path: BOOK_PAGE.path,
     name: BOOK_PAGE.name,
     component: BookPage,
+  },
+  {
+    path: BOOK_EDITOR_PAGE.path,
+    name: BOOK_EDITOR_PAGE.name,
+    component: BookEditor,
+  },
+  {
+    path: SERIES_EDITOR_PAGE.path,
+    name: SERIES_EDITOR_PAGE.name,
+    component: SeriesEditor,
   },
 ];
