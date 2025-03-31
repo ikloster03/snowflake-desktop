0226b72d0d0f3c1c51659fb30f4a6a0f86bbd846
Ivan Monastyrev <ikloster@yandex.ru>
2025-04-01 00:03:31 +0300

    fix: correct formatting and add missing semicolons in VitePress theme index for consistency

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,10 @@
-import DefaultTheme from 'vitepress/theme'
-import './custom.css'
+import DefaultTheme from 'vitepress/theme';
+import './custom.css';
 // import 'vitepress-plugin-mermaid/dist/theme.css'
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     // Регистрируем компоненты если необходимо
-  }
-}
+  },
+};
